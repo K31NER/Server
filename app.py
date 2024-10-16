e76480229d8986d7c9f0addd5b069035203acd83
K31NER <zunigaromerokeiner@gmail.com>
2024-10-16 13:22:45 -0500

    version 1.2
    se agrego la instalacion y configuracion de windows server

--- app.py
+++ app.py
@@ -32,6 +32,33 @@ with st.sidebar:
 st.header("Bienvenidos",divider="blue")
 st.write("Este proyecto de redes y comunicaciones se enfoca en la implementación de un servidor utilizando Windows Server 2022, con estaciones de trabajo configuradas en Windows 7 como clientes. El objetivo es demostrar la instalación y configuración de un entorno de red funcional, optimizando la conectividad y el manejo de servicios a través de un servidor centralizado.")
 
+st.subheader("Configuracion para la instalacion de windows server",divider="blue")
+with st.expander("ver capturas de el procedimiento👇"):
+    
+    st.subheader("Inicar instalacion")
+    st.image("instalar.png")
+    st.write("""
+        Al configurar la maquina virtual del windows server no cargamos la iso, solo ponemos el nombre personalizado y elejimos la version de 2022
+        """)
+    
+    st.subheader("Desactivar disquete")
+    st.image("disquete.png")
+    st.write("""
+        Desactivamos la opcion de disquete en las configuraciones virtual box de windows server 
+        """)
+    st.subheader("Subir iso de el windows server")
+    st.image("subir_iso.png")
+    st.write("""
+        Cargamos la iso de windows server despues de realizaer las configuraciones, ya que el inicio no se cargo
+        """)
+    
+    st.subheader("Red adaptador puente")
+    st.image("red_puente.png")
+    st.write("""
+        Activamos red de adaptdor puente en el server y el windows 7 para que tenga una correcta conexion
+        """)
+    
+
 st.subheader("Conexion entre server y cliente",divider="blue")
 st.video("https://youtu.be/sXWZtNF2ysQ?si=6JRXISVbwzk88k2d")
 with st.expander("Haz clic para ver la descripción 👇"):
@@ -101,4 +128,11 @@ with st.expander("Haz clic para ver la descripción 👇"):
     st.write("""
         En este paso vamos a compartir una carpeta con el mismo administrador de servidor como se mostro en 
         videos anteriores, solo que esta vez en lugar de elegir gruó users vamos a seleccionar el o los usuarios especificos, para que solo le salgan los archivos compartidos con ellos. Recordemos que debemos deshabilitar compartir los subfolders para que solo se comparta el principal.
+        """)
+
+st.subheader("Conexion server linux",divider="blue")
+st.video("https://youtu.be/32UOj2R9FJY?si=KAjBuwvBl8gPmQlv")
+with st.expander("Haz clic para ver la descripción 👇"):
+    st.write("""
+        En este paso vamos a verificar la conexion de linux con nuestro servidor de windows server, para eso debemos tener el IP de linux(ubunto 24.10) ponemos un IP que este en el mismo rango que el server para este caso es 192.168.20.22 y como ruta por defecto ponemos el IP de windows server y para verificar hacemos ping al IP y al nombre del dominio
         """)
